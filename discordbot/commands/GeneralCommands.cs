edfea8d57a7fb1f8182ad1265dad8f26d4c990d5
ZOOMEXX <whardsten@gmail.com>
2024-06-11 01:58:59 +0200

    Added working poll (!poll description(4 counts) Title).

--- discordbot/commands/GeneralCommands.cs
+++ discordbot/commands/GeneralCommands.cs
@@ -27,6 +27,81 @@ namespace discordbot.commands
 
         }
 
+        [Command("poll")]
+        public async Task Polll(CommandContext ctx, string option1, string option2, string option3, string option4, [RemainingText] string pollTitle)
+        {
+            var interactivity = Program.Client.GetInteractivity();
+            var pollTime = TimeSpan.FromSeconds(15);
+
+            DiscordEmoji[] emojiOptions = { DiscordEmoji.FromName(Program.Client, ":one:"),
+                                            DiscordEmoji.FromName(Program.Client, ":two:"),
+                                            DiscordEmoji.FromName(Program.Client, ":three:"),
+                                            DiscordEmoji.FromName(Program.Client, ":four:") };
+
+            string optionsDescription = $"{emojiOptions[0]} | {option1} \n" +
+                                        $"{emojiOptions[1]} | {option2} \n" +
+                                        $"{emojiOptions[2]} | {option3} \n" +
+                                        $"{emojiOptions[3]} | {option4}";
+
+            var pollMessage = new DiscordEmbedBuilder
+            {
+                Color = DiscordColor.Red,
+                Title = pollTitle,
+                Description = optionsDescription
+            };
+
+            var sentPoll = await ctx.Channel.SendMessageAsync(embed: pollMessage);
+            foreach (var emoji in emojiOptions)
+            {
+                await sentPoll.CreateReactionAsync(emoji);
+            }
+
+            var totalReactions = await interactivity.CollectReactionsAsync(sentPoll, pollTime);
+
+            int count1 = 0;
+            int count2 = 0;
+            int count3 = 0;
+            int count4 = 0;
+
+            foreach(var emoji in totalReactions)
+            {
+                if (emoji.Emoji == emojiOptions[0])
+                {
+                    count1++;
+                }
+                if (emoji.Emoji == emojiOptions[1])
+                {
+                    count2++;
+                }
+                if (emoji.Emoji == emojiOptions[2])
+                {
+                    count3++;
+                }
+                if (emoji.Emoji == emojiOptions[3])
+                {
+                    count4++;
+                }
+            }
+
+            int totalVotes = count1 + count2 + count3 + count4;
+            string resultsDescription = $"{emojiOptions[0]}: {count1} Votes \n" +
+                                        $"{emojiOptions[1]}: {count2} Votes \n" +
+                                        $"{emojiOptions[2]}: {count3} Votes \n" +
+                                        $"{emojiOptions[3]}: {count4} Votes \n\n" +
+                                        $"Total Votes: {totalVotes}";
+
+
+
+            var resultEmbed = new DiscordEmbedBuilder
+            {
+                Color = DiscordColor.Green,
+                Title = "Results of the poll",
+                Description = resultsDescription
+            };
+
+            await ctx.Channel.SendMessageAsync(embed: resultEmbed);
+        }
+
         [Command("test1")]
         public async Task Test1Command(CommandContext ctx)
         {
